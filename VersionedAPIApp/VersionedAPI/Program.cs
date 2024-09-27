f2ef5ae72527f8ef594479fc7876dd1c742f475d
Elston Misquitta <elston007@gmail.com>
2024-09-27 22:29:50 +0530

    Changes
    Changes

--- VersionedAPIApp/VersionedAPI/Program.cs
+++ VersionedAPIApp/VersionedAPI/Program.cs
@@ -1,11 +1,58 @@
 using Asp.Versioning;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-//[+] Logic
+
+//[+] Configer Swagger UI Section for Version Logic
+builder.Services.AddSwaggerGen(opts => 
+{
+    //(A) Variables
+    var title = "Our Versioned API";
+    var description = "API Documentation";
+    var terms = new Uri("https://localhost:7299/terms");
+    var license = new OpenApiLicense()
+    {
+        Name = "This is my Full liscence Information"
+    };
+    var contact = new OpenApiContact()
+    {
+        Name="Elston",
+        Email="elston007@gmailcom",
+        Url =new Uri ("https://courses.iamtimcorey.com/")
+    };
+
+    // (B) Set Values
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = $"{title} v1",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+
+
+    opts.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = $"{title} v2",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+
+});
+
+//[-] Configer Swagger UI Section for Version Logic
+
+
+
+//[+] Version Logic
 // Refer - https://www.milanjovanovic.tech/blog/api-versioning-in-aspnetcore
 builder.Services.AddApiVersioning(
     options =>
@@ -14,8 +61,19 @@ builder.Services.AddApiVersioning(
     options.ReportApiVersions = true;
     options.AssumeDefaultVersionWhenUnspecified = true;
 })
+.AddApiExplorer(opts =>   //[+] For Swagger Configuration
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
+})
 .AddMvc(); // This is needed for controllers (IMP)
-//[-] Logic
+//[-] Version Logic
+
+
+
+
+
+
 
 
 
@@ -25,13 +83,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    //[+] Configer Swagger UI Section for Version Logic
+    app.UseSwaggerUI( opts => 
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+    //[-] Configer Swagger UI Section for Version Logic
+
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
--- VersionedAPIApp/VersionedAPI/obj/Debug/net6.0/VersionedAPI.AssemblyInfo.cs
+++ VersionedAPIApp/VersionedAPI/obj/Debug/net6.0/VersionedAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VersionedAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fede4e7a2061611c2525f12c233bd35657eec787")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63e3068c20c88cd92a4433852db4a7499ec9108")]
 [assembly: System.Reflection.AssemblyProductAttribute("VersionedAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VersionedAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
