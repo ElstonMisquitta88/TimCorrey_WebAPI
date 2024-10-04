ae839e10b79065d32932809ed36f9550542f07f7
Elston Misquitta <elston007@gmail.com>
2024-10-04 11:20:54 +0530

    Protecting API
    Protecting API

---
+++ ApiProtectionApp/ApiProtection/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProtection.Models;
+
+public class UserModel
+{
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int ID { get; set; }
+
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(10)]
+    public string FirstName { get; set; }
+
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(10)]
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string EmailAddress { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Url]
+    public string HomePage { get; set; }
+
+    [Range(0, 5)]
+    public int NumberofVehicles { get; set; }
+}
