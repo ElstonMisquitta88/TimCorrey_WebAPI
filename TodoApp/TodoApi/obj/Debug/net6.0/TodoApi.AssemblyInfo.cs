7ab0800394afd710499797b1ce5ed2371ce65947
Elston Misquitta <elston007@gmail.com>
2024-10-27 00:05:08 +0530

    Changes
    Changes

--- TodoApp/TodoApi/obj/Debug/net6.0/TodoApi.AssemblyInfo.cs
+++ TodoApp/TodoApi/obj/Debug/net6.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a15f09845f40406c99f9a0523aa6974ae30e4a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e0fe43e838fe95560158605af1cdd0e70d3fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
